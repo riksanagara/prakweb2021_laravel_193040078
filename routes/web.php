d6073797f4020195fced5cc10d8b1b7a6197e084
unknown <riksakn00@gmail.com>
2021-10-07 18:45:14 +0700

    Belajar Laravel 8 Struktur Folder, Routes, & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Riksa Kusumah Nagara",
+        "email" => "193040078.mail.unpas.ac.id",
+        "image" => "profile.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
