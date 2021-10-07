84a117428aa0a7c7d62f986857d04e35886c81ab
unknown <riksakn00@gmail.com>
2021-10-07 19:53:22 +0700

    Belajar Laravel 8 Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Riksa Kusumah Nagara",
         "email" => "193040078.mail.unpas.ac.id",
         "image" => "profile.jpg"
@@ -26,5 +29,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Petama",
+            "slug" => "judul-post-pertama",
+            "author" => "Riksa Kusumah Nagara",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veritatis explicabo? Sed nostrum cum beatae sunt porro? Illum nobis beatae exercitationem inventore dicta, modi cumque incidunt quibusdam nam! Excepturi quidem culpa quia quibusdam, reiciendis officiis animi non totam delectus asperiores placeat quasi facilis minima tenetur dolor provident cum! Odit debitis aliquam corrupti voluptatem veniam enim placeat at reprehenderit a? Cum facere quas itaque, nam nisi qui nemo sit deserunt odio repellendus accusantium deleniti incidunt corrupti exercitationem perferendis modi vitae distinctio."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Ardan Herdiansyah",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis excepturi accusantium tempore. Vero corporis dignissimos incidunt sed deserunt, fugit ratione facere, doloremque quasi aut eveniet saepe dolor voluptas vel maxime, iusto ipsam temporibus voluptatem quisquam ipsa deleniti. Provident maxime dolore, quo optio recusandae doloribus quam fuga sit dicta, voluptatum consequuntur qui voluptatem iusto tempore excepturi officiis dolorum explicabo distinctio, id veniam voluptatibus nobis sequi animi soluta. Hic molestiae, optio nobis tempora provident minima? Sint molestias, ipsa libero perferendis commodi, pariatur dolor maiores vitae ab quae velit, nisi tempore omnis placeat dicta similique? Placeat aliquid optio non asperiores, fugit voluptate."
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Petama",
+            "slug" => "judul-post-pertama",
+            "author" => "Riksa Kusumah Nagara",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veritatis explicabo? Sed nostrum cum beatae sunt porro? Illum nobis beatae exercitationem inventore dicta, modi cumque incidunt quibusdam nam! Excepturi quidem culpa quia quibusdam, reiciendis officiis animi non totam delectus asperiores placeat quasi facilis minima tenetur dolor provident cum! Odit debitis aliquam corrupti voluptatem veniam enim placeat at reprehenderit a? Cum facere quas itaque, nam nisi qui nemo sit deserunt odio repellendus accusantium deleniti incidunt corrupti exercitationem perferendis modi vitae distinctio."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Ardan Herdiansyah",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis excepturi accusantium tempore. Vero corporis dignissimos incidunt sed deserunt, fugit ratione facere, doloremque quasi aut eveniet saepe dolor voluptas vel maxime, iusto ipsam temporibus voluptatem quisquam ipsa deleniti. Provident maxime dolore, quo optio recusandae doloribus quam fuga sit dicta, voluptatum consequuntur qui voluptatem iusto tempore excepturi officiis dolorum explicabo distinctio, id veniam voluptatibus nobis sequi animi soluta. Hic molestiae, optio nobis tempora provident minima? Sint molestias, ipsa libero perferendis commodi, pariatur dolor maiores vitae ab quae velit, nisi tempore omnis placeat dicta similique? Placeat aliquid optio non asperiores, fugit voluptate."
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post){
+        if($post["slug"] == $slug){
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
