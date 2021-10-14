5eaf5b59bdd2f0638b23ed210dfd930d8ee04a3d
unknown <riksakn00@gmail.com>
2021-10-14 19:59:31 +0700

    Belajar Laravel 8 Post Category & Eloquent Relationship

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <h1 class = "mb-5">Post Category : {{ $category }}</h1>
     @foreach ($posts as $post)
-    <article class = "mb-5">
+    <article>
         <h2>
             <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
         </h2>
