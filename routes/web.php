9d49fef3ed37a6d71ec96a2c56fc3e78be69d644
unknown <riksakn00@gmail.com>
2021-10-14 09:03:50 +0700

    Belajar Laravel 8 Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,54 +30,7 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Petama",
-            "slug" => "judul-post-pertama",
-            "author" => "Riksa Kusumah Nagara",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veritatis explicabo? Sed nostrum cum beatae sunt porro? Illum nobis beatae exercitationem inventore dicta, modi cumque incidunt quibusdam nam! Excepturi quidem culpa quia quibusdam, reiciendis officiis animi non totam delectus asperiores placeat quasi facilis minima tenetur dolor provident cum! Odit debitis aliquam corrupti voluptatem veniam enim placeat at reprehenderit a? Cum facere quas itaque, nam nisi qui nemo sit deserunt odio repellendus accusantium deleniti incidunt corrupti exercitationem perferendis modi vitae distinctio."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ardan Herdiansyah",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis excepturi accusantium tempore. Vero corporis dignissimos incidunt sed deserunt, fugit ratione facere, doloremque quasi aut eveniet saepe dolor voluptas vel maxime, iusto ipsam temporibus voluptatem quisquam ipsa deleniti. Provident maxime dolore, quo optio recusandae doloribus quam fuga sit dicta, voluptatum consequuntur qui voluptatem iusto tempore excepturi officiis dolorum explicabo distinctio, id veniam voluptatibus nobis sequi animi soluta. Hic molestiae, optio nobis tempora provident minima? Sint molestias, ipsa libero perferendis commodi, pariatur dolor maiores vitae ab quae velit, nisi tempore omnis placeat dicta similique? Placeat aliquid optio non asperiores, fugit voluptate."
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
 
 // halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Petama",
-            "slug" => "judul-post-pertama",
-            "author" => "Riksa Kusumah Nagara",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veritatis explicabo? Sed nostrum cum beatae sunt porro? Illum nobis beatae exercitationem inventore dicta, modi cumque incidunt quibusdam nam! Excepturi quidem culpa quia quibusdam, reiciendis officiis animi non totam delectus asperiores placeat quasi facilis minima tenetur dolor provident cum! Odit debitis aliquam corrupti voluptatem veniam enim placeat at reprehenderit a? Cum facere quas itaque, nam nisi qui nemo sit deserunt odio repellendus accusantium deleniti incidunt corrupti exercitationem perferendis modi vitae distinctio."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ardan Herdiansyah",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis excepturi accusantium tempore. Vero corporis dignissimos incidunt sed deserunt, fugit ratione facere, doloremque quasi aut eveniet saepe dolor voluptas vel maxime, iusto ipsam temporibus voluptatem quisquam ipsa deleniti. Provident maxime dolore, quo optio recusandae doloribus quam fuga sit dicta, voluptatum consequuntur qui voluptatem iusto tempore excepturi officiis dolorum explicabo distinctio, id veniam voluptatibus nobis sequi animi soluta. Hic molestiae, optio nobis tempora provident minima? Sint molestias, ipsa libero perferendis commodi, pariatur dolor maiores vitae ab quae velit, nisi tempore omnis placeat dicta similique? Placeat aliquid optio non asperiores, fugit voluptate."
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post){
-        if($post["slug"] == $slug){
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
