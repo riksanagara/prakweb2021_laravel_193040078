56b7402082889cbce10b7956caedc810f2fb4d35
unknown <riksakn00@gmail.com>
2021-10-29 22:34:04 +0700

    Belajar Laravel 8 View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
 
 
 /*
@@ -48,17 +49,6 @@ Route::get('/categories', function(){
     ]);
 });
 
-//     Route::get('/categories/{category:slug}', function(Category $category){
-//     return view('posts',[
-//         'title' => "Post By Category : $category->name",
-//         'active' => 'categories',
-//         'posts' => $category->posts->load('category', 'author')
-//     ]);
-// });
+Route::get('/login', [LoginController::class, 'index']);
 
-// Route::get('/authors/{author:username}', function(User $author){
-//     return view('posts',[
-//         'title' => "Posts By Author : $author->name",
-//         'posts' => $author->posts->load('category', 'author')
-//     ]);
-// });
+Route::get('/register', [RegisterController::class, 'index']);
