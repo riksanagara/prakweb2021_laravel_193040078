9c63d806f83e87f7eafba4522dd615c1a575fd91
unknown <riksakn00@gmail.com>
2021-10-29 15:55:55 +0700

    Belajar Laravel 8 Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -48,17 +48,17 @@ Route::get('/categories', function(){
     ]);
 });
 
-    Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts',[
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
+//     Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts',[
+//         'title' => "Post By Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
 
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts',[
-        'title' => "Posts By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/authors/{author:username}', function(User $author){
+//     return view('posts',[
+//         'title' => "Posts By Author : $author->name",
+//         'posts' => $author->posts->load('category', 'author')
+//     ]);
+// });
